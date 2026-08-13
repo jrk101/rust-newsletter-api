8a86e385143e192b2ed5ead0645173a3b43d04f0
Joseph Rithin KA <josephrithin2004@gmail.com>
2026-08-14 00:51:26 +0530

    First integration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,15 @@
-use rust_newsletter_api::main;
-#[test]
-fn dummy_test() {
-main()
+use rust_newsletter_api::run;
+
+#[tokio::test]
+async fn health_check_works(){
+    spawn_app();
+    let client = reqwest::Client::new();
+    let response = client.get("http://127.0.0.1:8000/healthcheck").send().await.expect("Request failed");
+    assert!(response.status().is_success());
+    assert_eq!(response.content_length(),Some(0));
+}
+
+fn spawn_app(){
+    let server=rust_newsletter_api::run().expect("Binding failed");
+    let _=tokio::spawn(server);
 }
