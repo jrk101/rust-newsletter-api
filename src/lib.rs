fd6cc90d43de40679e31186ef28156d799ec3279
Joseph Rithin KA <josephrithin2004@gmail.com>
2026-08-19 15:17:16 +0530

    Add POST /subscriptions endpoint with form-urlencoded parsing via serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
 use actix_web::{web, App, HttpServer, HttpResponse, Responder};
 use actix_web::dev::Server;
+use actix_web::web::Form;
 use std::net::TcpListener;
 
-pub async fn healthcheck() -> impl Responder{
-    HttpResponse::Ok()
+#[derive(serde::Deserialize)]
+struct FormData{
+    name:String,
+    email:String
+}
+
+pub async fn healthcheck() -> HttpResponse{
+    HttpResponse::Ok().finish()
+}
+pub async fn subscribe(_form:Form<FormData>) -> HttpResponse{
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener:TcpListener) -> Result<Server, std::io::Error>{
     let server = HttpServer::new(||{
         App::new().route("/healthcheck", web::get().to(healthcheck))
+        .route("/subscribe",web::post().to(subscribe))
     }).listen(listener)?.run();
     Ok(server)
 }
